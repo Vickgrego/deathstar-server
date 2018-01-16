65c14b365995ee6e7084bcdf1e2e35c76de5524b
Vickgrego <deadpoetmail@gmail.com>
2018-01-16 23:55:19 +0200

    fixed auth

--- auth.js
+++ auth.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
   if (typeof user === 'undefined' || user.name !== 'dark_force' || user.pass !== 'Qwerty1') {
     // We will discuss this line later in this section.
     res.header('WWW-Authenticate', 'Basic realm="Access to the API"');
-    return res.status(401).send({ error: 'Unauthorized' });
+    return res.status(401).send({ error: 'Empire is stronger than you!' });
   }
   next();
 };
